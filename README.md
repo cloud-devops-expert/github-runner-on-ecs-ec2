09e0c85d69f3616fe8fc588ce9cc99c0adc0fc3f
Miguel Goncalves <miguel.goncalves.v@gmail.com>
2023-01-13 16:28:48 +0000

    update README.md file with instructions

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
-# Welcome to your CDK TypeScript project
+# GitHub Runner running on ECS EC2 provider
 
-This is a blank project for CDK development with TypeScript.
+## Introduction
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+- the EC2 instance is the host for the GitHub Runners
+- the Docker is installed on the host
+- it requires permission changes to allow the call for non-root users from containers
 
-## Useful commands
+## Context
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+- the current project creates a test ECS cluster and runs the GitHub Runner from `myoung34/github-runner`
+- it is only a lab environment to examplify the required configurations by layer
+
+## Steps
+
+- ex.: containerUserId=1001
+- create a user account on the container image using a hardcoded user id:
+    - `sudo useradd -u **${containerUserId}** runner`
+- on the host launch configuration userdata, please put the command:
+    - `sudo setfacl --modify user:**${containerUserId}**:rw /var/run/docker.sock`
+- these instructions should fix the permission problems
+
+## Conclusion
+
+- most of the permission problems in this scenario come from host configurations and are not container specific
+- I have tested with e2e process and the results are consistent
+- the provided Dockerfile was too complex to put working on my local machine because contains several company specific
+  configurations, but the knowledge is transferable 
--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,16 +1,87 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from "constructs";
+import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  Cluster,
+  ContainerImage,
+  Ec2Service,
+  Ec2TaskDefinition,
+  LogDriver,
+  Secret,
+} from "aws-cdk-lib/aws-ecs";
+import { InstanceClass, InstanceSize, InstanceType } from "aws-cdk-lib/aws-ec2";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
+import { LogGroup } from "aws-cdk-lib/aws-logs";
 
-export class InfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class InfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const cluster = new Cluster(this, "Cluster");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const containerUserId = 1001; // the same user id as the container user - sudo useradd -u 1001 runner
+
+    cluster
+      .addCapacity("ec2", {
+        instanceType: InstanceType.of(
+          InstanceClass.BURSTABLE3_AMD,
+          InstanceSize.MICRO
+        ),
+        keyName: "c4h",
+        spotPrice: "0.05",
+      })
+      .addUserData(
+        `sudo setfacl --modify user:${containerUserId}:rw /var/run/docker.sock` // this line will update permissions on the host docker socket
+      );
+
+    /**
+     *  containerUserId must contain the id of the user runner on GitHub Runner
+     *  ex:
+     *    containerUserId=1001
+     *    sudo useradd -u 1001 runner
+     */
+
+    const taskDefinition = new Ec2TaskDefinition(this, "TaskDef");
+
+    taskDefinition.addVolume({
+      name: "docker",
+      host: {
+        sourcePath: "/var/run/docker.sock",
+      },
+    });
+
+    const containerDefinition = taskDefinition.addContainer("TheContainer", {
+      image: ContainerImage.fromRegistry("myoung34/github-runner"), //only for testing because your Dockerfile has as lot of internal things
+      memoryLimitMiB: 256,
+      privileged: true,
+      environment: {
+        REPO_URL: "https://github.com/cloud-devops-expert/aws-infra",
+        RUN_AS_ROOT: "false", // the container is using the runner user
+      },
+      secrets: {
+        ACCESS_TOKEN: Secret.fromSsmParameter(
+          StringParameter.fromSecureStringParameterAttributes(this, "PAT", {
+            parameterName: "PAT",
+          })
+        ),
+      },
+      logging: LogDriver.awsLogs({
+        streamPrefix: "github",
+        logGroup: new LogGroup(this, "ecs-github", {
+          logGroupName: "ecs/github/runner",
+        }),
+      }),
+    });
+
+    containerDefinition.addMountPoints({
+      readOnly: false,
+      containerPath: "/var/run/docker.sock",
+      sourceVolume: "docker",
+    });
+
+    new Ec2Service(this, "service", {
+      cluster,
+      taskDefinition,
+      desiredCount: 1,
+    });
   }
 }
